27bed060f834cf9cd892b3cdb469ad2d06f1dfb1
Md Seyam Ali <mdseyamali2005@gmail.com>
2026-02-27 12:48:25 +0600

    Assignment q2

--- Day_6/hw.py
+++ Day_6/hw.py
@@ -1,3 +1,8 @@
 ##q1########
 with open("name.txt", "w") as f:
     f.write("Seyam\nsufia\nkhatun\nali\nMira\n")
+#####q2########
+with open("log.txt","a") as f:
+    f.write("\nProgram run successfully")
+with open("log.txt","r") as f:
+    print(f.read())
